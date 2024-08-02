aeed26c6712a13dc3a07372f696f04fb7d4d548a
nadiduno <nadiduno@gmail.com>
2024-08-02 19:14:25 -0300

    Simulating the interaction with Alexa

--- main.py
+++ main.py
@@ -10,7 +10,22 @@ class LightBulb:
       self.state = False
       print("A lâmpada foi desligada.")
 
+#Silmulando a Biblioteca ```alexa ask-sdk
+def on_intent(intent_request):
+    intent_name = intent_request['intent']['name']
+
+    if intent_name == "LigarLampada":
+        light.turn_on()
+    elif intent_name == "DesligarLampada":
+        light.turn_of()
+
+
 # Criando uma instância da lâmpada
 light = LightBulb()
-light.turn_on()
+
+#light.turn_on()
+
+# Simulando a interação com a Alexa para ligar "Alexa LigarLampada"
+on_intent({'intent': {'name': 'LigarLampada'}})
+
 light.turn_of()
